a7ef7d7db528beef9e2191cbcf7f90e4de3cd49a
Evgen-s <y.shadrin@gmail.com>
2019-09-24 22:20:35 +0300

    создали структуру контроллеров

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 'use strict';
+
+var budgetController = (function () {
+  var x = 23;
+
+  var add = function (a) {
+    return x + a;
+  };
+
+  return {
+    publicTest: function (b) {
+      return add(b);
+    }
+  };
+
+})();
+
+var UIController = (function () {
+
+})();
+
+var controller = (function (BudgetCtrl, UICtrl) {
+
+  var z = BudgetCtrl.publicTest(6);
+
+  return {
+    anotherPublic: function () {
+      console.log(z);
+    }
+  };
+
+})(budgetController, UIController);
+
+
+controller.anotherPublic();
