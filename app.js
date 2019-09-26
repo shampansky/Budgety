80bc3ea820f24b1e5f6f69833d2d1ba5c285af7e
Evgen-s <y.shadrin@gmail.com>
2019-09-26 21:04:57 +0300

    добавил функцию инициализации

--- app.js
+++ app.js
@@ -34,12 +34,22 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (BudgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDomStrings();
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDomStrings();
+
+    document.querySelector(DOM.INPUT_BTN).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
   var ctrlAddItem = function () {
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
+
     // 2. Add the item to the budget controller
 
     // 3. Add the item to the UI
@@ -47,15 +57,14 @@ var controller = (function (BudgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-
   };
 
-  document.querySelector(DOM.INPUT_BTN).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function () {
+      console.log('Application has started');
+      setupEventListeners();
     }
-  });
-
+  };
 })(budgetController, UIController);
+
+controller.init();
