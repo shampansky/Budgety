194bf8c7ef570347d3ed456e4973fbc5181fc366
Evgen-s <y.shadrin@gmail.com>
2019-09-29 10:25:13 +0300

    добавил структуру данных в budgetController

--- app.js
+++ app.js
@@ -3,8 +3,36 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
 
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
 
 })();
+
+var Expense = function (id, description, value) {
+  this.id = id;
+  this.description = description;
+  this.value = value;
+};
 
 // UI CONTROLLER
 var UIController = (function () {
