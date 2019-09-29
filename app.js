c01c9dce5a4530fbbd55cadf4e2df18438542eb5
Evgen-s <y.shadrin@gmail.com>
2019-09-29 17:34:56 +0300

    добавил метод calculateBudget

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var budgetController = (function () {
     this.value = value;
   };
 
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.allItems[type].forEach(function (cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -23,7 +31,9 @@ var budgetController = (function () {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -50,6 +60,29 @@ var budgetController = (function () {
 
       // return new element
       return newItem;
+    },
+
+    calculateBudget: function () {
+      // calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+      // calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+      // calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+    },
+
+    getBudget: function () {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
     },
 
     testing: function () {
@@ -149,10 +182,11 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   var updateBudget = function () {
     // 1. Calculate the budget
-
+    budgetCtrl.calculateBudget();
     // 2. return the budget
-
+    var budget = budgetCtrl.getBudget();
     // 3. Display the budget on the UI
+    console.log(budget);
   };
 
   var ctrlAddItem = function () {
